40dda2e72beb8dfb343e9c75a7ee40407eae75b0
James Kelly <james.kelly.99@gmail.com>
2023-04-10 15:49:24 +1000

    remove the DAP configs because they don't work atm

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -3,85 +3,77 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "jay-babu/mason-nvim-dap.nvim", dependencies = {
-      "mfussenegger/nvim-dap", dependencies = {
-        "williamboman/mason.nvim", opts = overrides.mason,
-      }
-    }
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        --defaults
-        "vim",
-        "lua",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				--defaults
+				"vim",
+				"lua",
 
-        --web
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "markdown",
-        "markdown_inline",
+				--web
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"markdown",
+				"markdown_inline",
 
-        --server side
-        "c_sharp",
-        "sql"
-      }
-    }
-  },
+				--server side
+				"c_sharp",
+				"sql",
+			},
+		},
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  --load the debugger configurations into mason for easy install
-  {
-    "mfussenegger/nvim-dap",
-    "jay-babu/mason-nvim-dap.nvim"
-  }
-
+	--load the debugger configurations into mason for easy install
 }
 
 return plugins
