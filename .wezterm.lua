e143419064050ed22e33a809e758c84e23d8b732
James Kelly <james.kelly.99@gmail.com>
2024-10-05 09:13:48 +1000

    remove commented out stuff, and format

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,20 +19,6 @@ local function set_background_file(path)
 	}
 end
 
---this doens't work yet
-local function get_cpu_load()
-	if is_windows() then
-		local cmd = {"powershell -command \"Get-Counter '\\Processor(_Total)\\% Processor Time' | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue\""}
-		local process = wezterm.proc.spawn(cmd)
-		local success, output = process:wait()
-		if success then
-			return tonumber(output:match("%d+.%d+"))
-		else
-			return nil
-		end
-	end
-end
-
 wezterm.on('update-right-status', function(window)
 	local leader = ''
 	local date = wezterm.strftime '%H:%M %h %e'
@@ -40,7 +26,6 @@ wezterm.on('update-right-status', function(window)
 		leader = 'LEADER'
 	end
 
-	-- window:set_right_status(wezterm.format { { Text = leader .. ' ' .. date .. 'CPU: ' .. get_cpu_load() } })
 	window:set_right_status(wezterm.format { { Text = leader .. ' ' .. date  } })
 end)
 
@@ -51,68 +36,56 @@ config.use_fancy_tab_bar = true
 config.show_tabs_in_tab_bar = true
 config.show_new_tab_button_in_tab_bar = true
 
---this needs more work
--- config.launch_menu = {
--- 	{
--- 		label = 'Edge',
--- 		args = { 'powershell -start microsoft-edge:http://google.com' },
--- 	},
--- 	{
--- 		label = 'FireFox',
--- 		args = { 'powershell -start firefox' },
--- 	}
--- }
---
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1500 }
 
--- --rename tab set to CTRL|SHIFT R
+-- rename tab set to CTRL|SHIFT R
 config.keys = {
-{
-	key = 'R',
-	mods = 'CTRL|SHIFT',
-	action = act.PromptInputLine {
-		description = 'Enter new name for tab',
-		action = wezterm.action_callback(function(window, pane, line)
-			-- line will be `nil` if they hit escape without entering anything
-			-- An empty string if they just hit enter
-			-- Or the actual line of text they wrote
-			if line then
-				window:active_tab():set_title(line)
-			end
-		end),
+	{
+		key = 'R',
+		mods = 'CTRL|SHIFT',
+		action = act.PromptInputLine {
+			description = 'Enter new name for tab',
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		},
 	},
-},
-{ key = 'F11',						action = act.ToggleFullScreen },
--- Send C-a when pressing C-a twice
-{ key = "a", mods = "LEADER",       action = act.SendKey { key = "a", mods = "CTRL" } },
-{ key = "c", mods = "LEADER",       action = act.ActivateCopyMode },
+	{ key = 'F11',						action = act.ToggleFullScreen },
+	-- Send C-a when pressing C-a twice
+	{ key = "a", mods = "LEADER",       action = act.SendKey { key = "a", mods = "CTRL" } },
+	{ key = "c", mods = "LEADER",       action = act.ActivateCopyMode },
 
--- Pane keybindings
-{ key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain"} },
-{ key = "\\", mods = "LEADER",       action = act.SplitHorizontal { domain = "CurrentPaneDomain"} },
-{ key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
-{ key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
-{ key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
-{ key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
-{ key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
-{ key = "z", mods = "LEADER",       action = act.TogglePaneZoomState },
-{ key = "s", mods = "LEADER",       action = act.RotatePanes "Clockwise" },
--- We can make separate keybindings for resizing panes
--- But Wezterm offers custom "mode" in the name of "KeyTable"
-{ key = "r", mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+	-- Pane keybindings
+	{ key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain"} },
+	-- maps | to split horizontal
+	{ key = "\\", mods = "LEADER",       action = act.SplitHorizontal { domain = "CurrentPaneDomain"} },
+	{ key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
+	{ key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
+	{ key = "z", mods = "LEADER",       action = act.TogglePaneZoomState },
+	{ key = "s", mods = "LEADER",       action = act.RotatePanes "Clockwise" },
+	-- We can make separate keybindings for resizing panes
+	-- But Wezterm offers custom "mode" in the name of "KeyTable"
+	{ key = "r", mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
 
--- Tab keybindings
-{ key = "n", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
-{ key = "[", mods = "LEADER",       action = act.ActivateTabRelative(-1) },
-{ key = "]", mods = "LEADER",       action = act.ActivateTabRelative(1) },
-{ key = "t", mods = "LEADER",       action = act.ShowTabNavigator },
--- Key table for moving tabs around
-{ key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+	-- Tab keybindings
+	{ key = "n", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "[", mods = "LEADER",       action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "LEADER",       action = act.ActivateTabRelative(1) },
+	{ key = "t", mods = "LEADER",       action = act.ShowTabNavigator },
+	-- Key table for moving tabs around
+	{ key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
 
--- Lastly, workspace
-{ key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+	-- Lastly, workspace
+	{ key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
 }
-
 
 config.inactive_pane_hsb = {
 	brightness = 0.4
@@ -152,6 +125,6 @@ if is_windows() then
 else
 	config.default_prog = { 'bash' }
 	set_background_file("/home/james/.config/wezterm/nord.png")
-	config.font_size = 13
+	config.font_size = 11
 end
 return config
