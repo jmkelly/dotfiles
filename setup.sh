8c695404367c7ecd26ac4a060b618a360f5fd8ad
James Kelly <james.kelly.99@gmail.com>
2025-04-19 09:55:01 +1000

    more echo statements and install dotnet-sdk-9.0

--- setup.sh
+++ setup.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-# Use the latest Ubuntu LTS as the base image
-# from ubuntu:24.04
+# Use the latest Ubuntu 24.04 LTS as the base 
 # shell script to setup a base dev setup on host
 
-#some setup
-#clone my neovim config quick start
-
-
 sudo apt update && sudo apt install -y curl git #git most likely will already be installed, but just in case it isn't
+sudo add-apt-repository ppa:dotnet/backports
+
 TEMP_DIR=/tmp/dotfiles
+
 mkdir -p $TEMP_DIR
 mkdir -p $HOME/.config/wezterm
+
+# clone my dotfiles which to a tmp directory for moving configs around
+#
 git clone https://github.com/jmkelly/dotfiles.git $TEMP_DIR && \
 	#backup existing nvim config just in case if it exists
 [ -d $HOME/.config/nvim ] && mv $HOME/.config/nvim $HOME/.config/nvim_bak_$(date +%Y%m%d%H%M%S)
 		
+
 cp $TEMP_DIR/.config/nvim $HOME/.config/nvim -R && \
-    cp $TEMP_DIR/.wezterm.lua $HOME/.config/wezterm/wezterm.lua && \
-		cp $TEMP_DIR/.tmux.conf $HOME/.tmux.conf && \
-		cp $TEMP_DIR/nord.png $HOME/.config/wezterm/nord.png
+cp $TEMP_DIR/.wezterm.lua $HOME/.config/wezterm/wezterm.lua && \
+cp $TEMP_DIR/.tmux.conf $HOME/.tmux.conf && \
+cp $TEMP_DIR/nord.png $HOME/.config/wezterm/nord.png
+
+echo "installed neovim config to $HOME/.config/nvim"
+echo "installed wezterm config to $HOME/.config/wezterm"
+echo "installed tmux config to $HOME/.tmux.conf"
 
 #install wezterm
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+echo "installed wezterm"
 
 # Update the package list and install necessary packages
-sudo apt-get update && sudo apt-get install -y software-properties-common wget npm fontconfig unzip ripgrep fd-find dotnet-sdk-8.0 wezterm tmux && sudo rm -rf /var/lib/apt/lists/*
+# we are install dotnet sdk 8 and 9 so we have both the LTS and current version
+sudo apt-get update && sudo apt-get install -y software-properties-common wget npm fontconfig unzip ripgrep fd-find dotnet-sdk-9.0 dotnet-sdk-8.0 wezterm tmux && sudo rm -rf /var/lib/apt/lists/*
+echo "installed packages"
 
 # Install a Nerd Font (e.g., FiraCode Nerd Font)
 FONT_NAME=JetBrainsMono
@@ -37,6 +46,8 @@ sudo mkdir -p /usr/share/fonts/nerdfonts &&
 	sudo unzip /tmp/$FONT_NAME.zip -d /usr/share/fonts/nerdfonts/ &&  fc-cache -fv && 
 	sudo rm /tmp/$FONT_NAME.zip
 
+echo "installed nerd font $FONT_NAME $FONT_VERSION"
+
 
 # install lazygit from source
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
@@ -44,6 +55,8 @@ curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/downl
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
 sudo rm lazygit.tar.gz
+
+echo "installed lazygit $LAZYGIT_VERSION"
 
 
 NEOVIM_VERSION=0.10.4
@@ -69,9 +82,9 @@ echo installed neovim to $INSTALL_DIR
 PWSH_VERSION=7.4.6
 wget "https://github.com/PowerShell/PowerShell/releases/download/v$PWSH_VERSION/powershell_$PWSH_VERSION-1.deb_amd64.deb"
 sudo dpkg -i powershell_$PWSH_VERSION-1.deb_amd64.deb
-echo installed powershell
 sudo rm powershell_$PWSH_VERSION-1.deb_amd64.deb
 
+echo "installed powershell $PWSH_VERSION"
 #
 # GCM_VERSION=2.6.0
 # GCM_APP="gcm-linux_amd64.$GCM_VERSION.deb"
