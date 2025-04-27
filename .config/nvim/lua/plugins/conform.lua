902516a6bec28ff78fe67feb6ef895f7b90ace05
James Kelly <james.kelly.99@gmail.com>
2025-04-27 10:27:45 +1000

    format

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,15 +1,15 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				cs = { "csharpier" },
-			},
-			format_after_save = {
-				lsp_format = "fallback",
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                cs = { "csharpier" },
+            },
+            format_after_save = {
+                lsp_format = "fallback",
+            },
+        })
+    end,
 }
