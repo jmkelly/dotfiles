2d5791477fc17166b3167fa1fb04ddbaa59560ef
James Kelly <james.kelly.99@gmail.com>
2025-05-02 20:46:45 +1000

    add path to lualine, makes it easier when editing razor pages

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,10 @@ return {
         sections = {
             lualine_a = { "mode" },
             lualine_b = { "branch", "diff" },
-            lualine_c = { "filename", "diagnostics" },
+            lualine_c = { {
+                "filename",
+                path = 1,
+            }, "diagnostics" },
             lualine_y = { "lsp_status" },
             lualine_z = { "location" },
         },
