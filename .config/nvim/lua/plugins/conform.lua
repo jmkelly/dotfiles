7a4a39406bdcc87c2e19ef855c41b7793122417d
James Kelly <james.kelly.99@gmail.com>
2025-05-02 20:47:17 +1000

    workaround

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,11 +1,20 @@
 return {
     "stevearc/conform.nvim",
+    --dir = "~/Documents/code/conform.nvim",
+    --name = "conform.nvim",
     opts = {},
     config = function()
         require("conform").setup({
             formatters_by_ft = {
                 lua = { "stylua" },
-                cs = { "csharpier" },
+                cs = {
+                    csharpier = {
+                        --workaround until https://github.com/stevearc/conform.nvim/pull/695 is merged
+                        command = "csharpier",
+                        args = "format",
+                    },
+                },
+                sql = { "sqruff" },
             },
             format_after_save = {
                 lsp_format = "fallback",
